c525f8bf009190473b65e1c0affa39e6068cbda9
Sichen (Shawn) Chao <shawns.chao@gmail.com>
2020-06-28 22:11:50 -0500

    Replace Data and Script
    Incorporated code/data to label the week number of the tournament

--- Scraper/player_games_scraper_womens_singles.py
+++ Scraper/player_games_scraper_womens_singles.py
@@ -62,6 +62,60 @@ def fetch_valid_years_person(player_tournament_soup):
                 year -= 1
     return valid_years_list
 
+def score(bwf_score_string):
+    try:
+        forbidden = ['', 'Walkover']
+        if bwf_score_string in forbidden:
+            return [None, None]
+        if 'Walkover, ' in bwf_score_string:
+            bwf_score_string = bwf_score_string.split('Walkover, ')[1]
+        score_list = bwf_score_string.split(', ')
+        player_score = 0
+        opponent_score = 0
+        for game in score_list:
+            game_score = game.split('-')
+            player_score += int(game_score[0])
+            opponent_score += int(game_score[1])
+        return [player_score, opponent_score]
+    except:
+        print(bwf_score_string)
+        return [None, None]
+
+def month_string_to_number(string):
+    m = {
+        'jan': 1,
+        'feb': 2,
+        'mar': 3,
+        'apr':4,
+         'may':5,
+         'jun':6,
+         'jul':7,
+         'aug':8,
+         'sep':9,
+         'oct':10,
+         'nov':11,
+         'dec':12
+        }
+    s = string.strip()[:3].lower()
+
+    try:
+        out = m[s]
+        return out
+    except:
+        raise ValueError('Not a month')
+
+def get_week(df):
+    df['Week'] = df['Week'].str.split(', ').str[0] #get rid of the country
+    for index, row in df.iterrows():
+        week_list = row['Week'].split(' ')
+        start_day = int(week_list[0])
+        try: #if tournament goes between two months based on formatting. This comes first
+            month_number = month_string_to_number(week_list[1]) #month start date
+        except ValueError: #if tournament on goes on within one month
+            month_number = month_string_to_number(week_list[-1])
+        df.loc[index, 'Week Number'] = datetime.date(int(row['Year']), month_number, start_day).strftime("%V")
+    return df
+
 df_matches = pd.DataFrame(columns = ['Tournament Name', 'Match Type','Year', 'Week','Player',
                                                            "Opponent", 'Score', 'Result (W/L)', 'Time',
                                                            'Player BWF Link', 'Opponent BWF Link'])
@@ -76,9 +130,10 @@ for page_num in tqdm(range(page_num_csv+1)): #singles and add one bc python inde
     badminton_soup = fetch_ranking_week_year_singles_soup(year, week, page_num+1)
     players_soup = badminton_soup.findAll("div", { "class" : "player" }) #players on each page
     # for player_soup in players_soup:
-    # for player_num_page in tqdm(range(player_num_page_csv+1,len(players_soup))): #add one because of python indexing
+    # for player_num_page in tqdm(range(player_num_page_csv + 1, len(players_soup))):  # add one because of python indexing
     for player_num_page in tqdm(range(len(players_soup))):
-        if player_num_page%30 == 5:
+    # for player_num_page in tqdm(range(player_num_page_csv+1,len(players_soup))): #add one because of python indexing
+        if player_num_page%50 == 5:
             driver.quit()
             time.sleep(75)
             driver = webdriver.Chrome('C:\webdrivers\chromedriver.exe')
@@ -108,6 +163,7 @@ for page_num in tqdm(range(page_num_csv+1)): #singles and add one bc python inde
             tournaments_split_valid_soups = [BeautifulSoup(tournaments_split_valid_soups_str,'html.parser')
                                              for tournaments_split_valid_soups_str in tournaments_split_valid_soups_strs]
             for tournament_soup in tournaments_split_valid_soups: #each tournament
+                tournament_match_number = 1
                 # tournament_event = tournament_soup.findAll('div', {'class': 'title-tournament-mat'
                 #                                                              'ches'})[0].find('a').text
                 # if "Singles" not in tournament_event: #doubles or mixed match
@@ -115,13 +171,14 @@ for page_num in tqdm(range(page_num_csv+1)): #singles and add one bc python inde
                 box_profile_tournament = tournament_soup.findAll('div', {'class': 'box-profile-tournament'})[0]
                 tournament_name = box_profile_tournament.find('a')['title']
                 # print(tournament_event,tournament_name)
-                tournament_day = box_profile_tournament.find("h4").text
+                tournament_day = box_profile_tournament.find("h4").text.split(', ')[0]
+                #week of tournament and sometimes includes a country after a comma
                 tournament_year = valid_years
                 match_types = tournament_soup.findAll('div', {'class': 'title-tournament-matches'})
                 # match_type = tournament_soup.findAll('div', {'class': 'title-tournament-matches'})[0].find('a').text
                 tournament_matches_set = tournament_soup.findAll('div', {'class': 'tournament-matches'})
                 for i in range(len(tournament_matches_set)): #each set of matches (prelim rounds and main draw)
-                    #use so that the match type matches up
+                    tournament_match_number_type = 1
                     tournament_matches = tournament_matches_set[i]
                     tournament_matches_rows = tournament_matches.findAll('div',{'class':'tournament-matches-row'})
                     for tournament_match in tournament_matches_rows: #each match
@@ -135,31 +192,44 @@ for page_num in tqdm(range(page_num_csv+1)): #singles and add one bc python inde
                         player = player_names[0]
                         opponent = player_names[1]
                         opponent_link = names_soup[1].find('a')['href']
-                        score = tournament_match.find('span').text
+                        bwf_score = tournament_match.find('div',{'class':'player-result-win'}).find('span').text
+                        player_points = score(bwf_score)[0]
+                        opponent_points = score(bwf_score)[1]
+                        # score = tournament_match.find('span').text
                         result = tournament_match.find('strong').text
                         length = tournament_match.findAll("div", { "class" : "timer" })[0].text
 
                         df_match = pd.DataFrame([[tournament_name, match_type, tournament_year, tournament_day, player,
-                                                  opponent,score,result,length, player_html_link, opponent_link]],
+                                                  opponent,bwf_score,player_points, opponent_points, result,length,
+                                                  player_html_link, opponent_link, tournament_match_number, tournament_match_number_type]],
                                                 columns = ['Tournament Name', 'Match Type','Year', 'Week','Player',
-                                                           "Opponent", 'Score', 'Result (W/L)', 'Time',
-                                                           'Player BWF Link', 'Opponent BWF Link'])
+                                                           "Opponent", 'Score', 'Player Points Scored', 'Opponent Points Scored',
+                                                           'Result (W/L)', 'Time',
+                                                           'Player BWF Link', 'Opponent BWF Link', 'Tournament Match Number', 'Match Type Number'])
                         # print(df_match)
                         # cols = ['Tournament Name', 'Year', 'Week','Player',
                                                            # "Opponent", 'Score', 'Result (W/L)', 'Time']
                         # merged = pd.merge(df_matches,df_match, on = cols, how ='outer', indicator = True)
                         # df_matches = pd.concat([df_matches,merged.iloc[merged._merge=='left_only', cols]])
                         df_matches = pd.concat([df_matches, df_match])
+                        tournament_match_number += 1
+                        tournament_match_number_type += 1
         # print(df_matches.tail(10))
         filelist_to_remove = glob.glob(f'C:\\Users\\shawn\\Desktop\\Personal Coding Projects'
                        f'\\Badminton Rankings\\Data\\womens_singles_matches_*.csv')
         for file_to_remove in filelist_to_remove:
             os.remove(file_to_remove)
         df_matches.to_csv(f'C:\\Users\\shawn\\Desktop\\Personal Coding Projects'
-                        f'\\Badminton Rankings\\Data\\womens_singles_matches_{year}_{week}_{page_num}_{player_num_page}.csv', index=False)
+                        f'\\Badminton Rankings\\Data\\womens_singles_matches_type_{year}_{week}_{page_num}_{player_num_page}.csv',
+                          index=False)
 
 driver.quit()
+
 df_matches_drop_dup = df_matches.drop_duplicates(subset=['Tournament Name', 'Year', 'Week', 'Player',
-                                                         "Opponent", 'Score', 'Result (W/L)', 'Time'], keep='first')
+                                                         "Opponent", 'Score', 'Result (W/L)', 'Time', 'Match Type',
+                                                         'Tournament Match Number', 'Match Type Number'], keep='first')
+df_matches_drop_dup = get_week(df_matches_drop_dup)
+
 df_matches_drop_dup.to_csv(f'C:\\Users\\shawn\\Desktop\\Personal Coding Projects'
-                           f'\\Badminton Rankings\\Data\\womens_singles_matches_{year}_{week}_total.csv', index = False)
+                           f'\\Badminton Rankings\\Data\\womens_singles_matches_type_{year}_{week}_scraped_total.csv',
+                           index = False)
